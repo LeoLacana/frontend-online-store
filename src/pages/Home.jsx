943ebabe36765e2247035c5f3d4dea2b569b5b17
LeoLacana <wilson17414@gmail.com>
2021-06-15 18:09:00 -0300

    requisito 4

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Categories from './Categories';
 
 export default class Home extends Component {
   render() {
@@ -10,6 +11,7 @@ export default class Home extends Component {
         <h2 data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
         </h2>
+        <Categories />
       </div>
     );
   }
